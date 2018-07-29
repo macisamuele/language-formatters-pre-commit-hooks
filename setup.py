e2bad626bf48a01c0ca87500aa9c2f4eb5b49dfb
Samuele Maci <macisamuele@gmail.com>
2018-07-29 23:03:33 +0200

    Add CLI tools for pretty format INI and rust

--- setup.py
+++ setup.py
@@ -43,6 +43,8 @@ setup(
             'pretty-format-golang = language_formatters_pre_commit_hooks.pretty_format_golang:pretty_format_golang',
             'pretty-format-java = language_formatters_pre_commit_hooks.pretty_format_java:pretty_format_java',
             'pretty-format-kotlin = language_formatters_pre_commit_hooks.pretty_format_kotlin:pretty_format_kotlin',
+            'pretty-format-ini = language_formatters_pre_commit_hooks.pretty_format_ini:pretty_format_ini',
+            'pretty-format-rust = language_formatters_pre_commit_hooks.pretty_format_rust:pretty_format_rust',
             'pretty-format-toml = language_formatters_pre_commit_hooks.pretty_format_toml:pretty_format_toml',
             'pretty-format-yaml = language_formatters_pre_commit_hooks.pretty_format_yaml:pretty_format_yaml',
         ],
