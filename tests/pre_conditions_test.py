614340a6a364fc2285796d60ef2ef81ea09d9ca4
Samuele Maci <macisamuele@gmail.com>
2018-07-29 12:18:08 +0200

    Fix tests

---
+++ tests/pre_conditions_test.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import mock
+import pytest
+
+from language_formatters_pre_commit_hooks.pre_conditions import _assert_command_succeed
+from language_formatters_pre_commit_hooks.pre_conditions import golang_required
+from language_formatters_pre_commit_hooks.pre_conditions import java_required
+
+
+@pytest.fixture(params=[True, False])
+def success(request):
+    with mock.patch(
+        'language_formatters_pre_commit_hooks.pre_conditions.run_command',
+        autospec=True,
+        return_value=(0 if request.param else 1, None),
+    ):
+        yield request.param
+
+
+def test___assert_command_succeed(success):
+    raised_exception = None
+    try:
+        _assert_command_succeed('command', 'assert_message')
+    except AssertionError as e:
+        raised_exception = e
+
+    if success:
+        assert raised_exception is None
+    else:
+        assert isinstance(raised_exception, AssertionError) and 'assert_message' == str(raised_exception)
+
+
+@pytest.mark.parametrize(
+    'decorator, assert_content',
+    [
+        [java_required, 'JRE is required'],
+        [golang_required, 'golang/gofmt is required'],
+    ],
+)
+def test_tool_required(success, decorator, assert_content):
+    @decorator
+    def func():
+        pass
+
+    raised_exception = None
+    try:
+        func()
+    except AssertionError as e:
+        raised_exception = e
+
+    if success:
+        assert raised_exception is None
+    else:
+        assert isinstance(raised_exception, AssertionError) and assert_content in str(raised_exception)
--- tests/pretty_format_java_test.py
+++ tests/pretty_format_java_test.py
@@ -3,22 +3,26 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import os
 import shutil
 
 import pytest
 
 from language_formatters_pre_commit_hooks.pretty_format_java import pretty_format_java
+from tests.conftest import change_dir_context
+from tests.conftest import undecorate_function
 
 
 @pytest.fixture(autouse=True)
 def change_dir():
-    working_directory = os.getcwd()
-    try:
-        os.chdir('test-data/pretty_format_java/')
+    with change_dir_context('test-data/pretty_format_java/'):
         yield
-    finally:
-        os.chdir(working_directory)
+
+
+@pytest.fixture
+def undecorate_method():
+    # Method undecoration is needed to ensure that tests could be executed even if the tool is not installed
+    with undecorate_function(pretty_format_java) as undecorated:
+        yield undecorated
 
 
 @pytest.mark.parametrize(
@@ -27,18 +31,18 @@ def change_dir():
         ('invalid.java', 1),
     ),
 )
-def test_pretty_format_java(filename, expected_retval):
-    assert pretty_format_java([filename]) == expected_retval
+def test_pretty_format_java(undecorate_method, filename, expected_retval):
+    assert undecorate_method([filename]) == expected_retval
 
 
-def test_pretty_format_java_autofix(tmpdir):
+def test_pretty_format_java_autofix(tmpdir, undecorate_method):
     srcfile = tmpdir.join('to_be_fixed.java')
     shutil.copyfile(
         'invalid.java',
         srcfile.strpath,
     )
-    assert pretty_format_java(['--autofix', srcfile.strpath]) == 1
+    assert undecorate_method(['--autofix', srcfile.strpath]) == 1
 
     # file was formatted (shouldn't trigger linter again)
-    ret = pretty_format_java([srcfile.strpath])
+    ret = undecorate_method([srcfile.strpath])
     assert ret == 0
