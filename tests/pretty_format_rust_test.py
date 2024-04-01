a87b9d4ad2f31b78ad285fdd66e6027254caa28c
AJIOB <aleksandr9809@gmail.com>
2024-04-01 10:50:43 +0300

    Add support the custom manifest specification (with tests)

--- tests/pretty_format_rust_test.py
+++ tests/pretty_format_rust_test.py
@@ -30,16 +30,49 @@ def undecorate_method():
         ("pretty-formatted/src/main.rs", 0),
         ("not-pretty-formatted/src/main.rs", 1),
         ("not-pretty-formatted_fixed/src/main.rs", 0),
+        ("not-pretty-formatted_subdir/src/bin/src/main.rs", 1),
     ),
 )
 def test_pretty_format_rust(undecorate_method, filename, expected_retval):
+    manifest_root = filename.split("/")[0]
+    manifest_root = os.path.abspath(manifest_root)
     filename = os.path.abspath(filename)
-    x = os.path.dirname(os.path.dirname(filename))
-    print(x)
-    with change_dir_context(x):
+
+    with change_dir_context(manifest_root):
         assert undecorate_method([filename]) == expected_retval
+
+
+@pytest.mark.parametrize(
+    ("filename", "expected_retval"),
+    (
+        ("invalid/src/main.rs", 1),
+        ("pretty-formatted/src/main.rs", 0),
+        ("not-pretty-formatted/src/main.rs", 1),
+        ("not-pretty-formatted_fixed/src/main.rs", 0),
+        ("not-pretty-formatted_subdir/src/bin/src/main.rs", 1),
+    ),
+)
+def test_pretty_format_rust_manifest(undecorate_method, filename, expected_retval):
+    manifest_root = filename.split("/")[0]
+    manifest_root = os.path.abspath(manifest_root)
+    filename = os.path.abspath(filename)
+
+    manifest_file = os.path.join(manifest_root, "Cargo.toml")
+    print(manifest_file)
+    assert undecorate_method(["--manifest-path", manifest_file, filename]) == expected_retval
 
 
 def test_pretty_format_rust_autofix(tmpdir, undecorate_method):
     copyfile("not-pretty-formatted/Cargo.toml", tmpdir.join("Cargo.toml").strpath)
     run_autofix_test(tmpdir, undecorate_method, "not-pretty-formatted/src/main.rs", "not-pretty-formatted_fixed/src/main.rs")
+
+
+def test_pretty_format_rust_autofix_subdir(tmpdir, undecorate_method):
+    copyfile("not-pretty-formatted_subdir/Cargo.toml", tmpdir.join("Cargo.toml").strpath)
+    run_autofix_test(
+        tmpdir.mkdir("src").mkdir("bin"),
+        undecorate_method,
+        "not-pretty-formatted_subdir/src/bin/src/main.rs",
+        "not-pretty-formatted_fixed/src/main.rs",
+        ["--manifest-path", "../../Cargo.toml"],
+    )
