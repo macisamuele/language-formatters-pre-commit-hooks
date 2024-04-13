a93b53fd5f9d632435e0e2e659e0b0ad8b000bef
Eir Nym <485399+eirnym@users.noreply.github.com>
2024-04-13 01:11:37 -0700

    Update pretty_format_java_test.py
    Co-authored-by: Max R <mxr@users.noreply.github.com>

--- tests/pretty_format_java_test.py
+++ tests/pretty_format_java_test.py
@@ -52,7 +52,7 @@ def test__download_google_java_formatter_jar(ensure_download_possible, version):
 )
 @pytest.mark.integration
 def test__download_palantir_java_formatter_jar(ensure_download_possible, version):  # noqa: F811
-    # Test that we can download different version of the Google Java Formatter
+    # Test that we can download different version of the Palantir Java Formatter
     _download_palantir_java_formatter_jar(version)
 
 
